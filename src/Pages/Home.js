28d330b2b83e07bc17f4afe00ca323833768f7ac
Rikotee <teerek81@gmail.com>
2022-11-16 17:25:50 +0200

    widget selector

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
 import '../components/style.css'
-import home from '../components/image/home.svg';
+
 
 const Home = () => {
 
-  return (
+   // State with list of all checked item
+   const [checked, setChecked] = useState([]);
+   const checkList = [
+    {id: 1, name: "Events"},
+    {id: 2, name: "News"},
+    {id: 3, name: "HSL"},
+  ];
+ 
+   // Add/Remove checked item from list
+   const handleCheck = (event) => {
+     var updatedList = [...checked];
+     if (event.target.checked) {
+       updatedList = [...checked, event.target.value];
+     } else {
+       updatedList.splice(checked.indexOf(event.target.value), 1);
+     }
+     setChecked(updatedList);
+   };
+ 
+   // Generate string of checked items
+   const checkedItems = checked.length
+     ? checked.reduce((total, item) => {
+         return total + ", " + item;
+       })
+     : "";
+ 
+   // Return classes based on whether item is checked
+   var isChecked = (item) =>
+     checked.includes(item) ? "checked-item" : "not-checked-item";
 
-    <div>
-      <h1>Homepage</h1>
-        <image>
-          <img src={home}  alt="Logo" />
-        </image>
+  return (
+    <div className="app">
+      <div className="checkList">
+        <div className="title">Your WidgetList:</div>
+        <div className="list-container">
+          {checkList.map((item, index) => (
+            <div key={index}>
+              <input value={item.name} type="checkbox" onChange={handleCheck} />
+              <span className={isChecked(item.name)}>{item.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        {`Items checked are: ${checkedItems}`}
+      </div>
+
     </div>
   );
 };
