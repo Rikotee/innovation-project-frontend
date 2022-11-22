7f53e4ccc3d06df5c18afd395e548ebd8d35dcd2
Rikotee <teerek81@gmail.com>
2022-11-22 16:07:57 +0200

    widget shown when checkbox

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import '../components/style.css'
 import EventsList from "../components/events";
-import WidgetSelector from "../components/widgetSelector";
-
 
 const Home = () => {
 
   const events = useLaunches()
+
+   // State with list of all checked item
+   const [checked, setChecked] = useState([]);
+
+  // Add/Remove checked item from list
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+      if (event.target.checked) {
+        updatedList = [...checked, event.target.value];
+      } else {
+        updatedList.splice(checked.indexOf(event.target.value), 1);
+      }
+      setChecked(updatedList);
+     };
+       
+  // Return classes based on whether item is checked
+  var isChecked = (item) =>
+    checked.includes(item) ? "checked-item" : "not-checked-item";
 
    const checkList = [
     {id: 1, name: "Events"},
     {id: 2, name: "News"},
     {id: 3, name: "HSL"},
   ];
+
+/*   const listItems = checked.map((widgetList)=>{
+    return <li>{widgetList}</li>;   
+  }); */
+
+  const eventWidget = (checked) =>{
+    if (checked.includes('Events')) {
+      return <div><EventsList data={events} /></div>;  
+    }
+  };
  
   return (
     <div className="app">
 
-      <div>
-        <WidgetSelector data={checkList} />
-      </div>
+<div className="checkList">
+    <div className="title">Your WidgetList:</div>
+    <div className="list-container">
+      {checkList.map((item, index) => (
+        <div key={index}>
+          <input value={item.name} type="checkbox" onChange={handleCheck} />
+          <span className={isChecked(item.name)}>{item.name}</span>
+        </div>
+      ))}
+    </div>
 
-      <div>
+  </div>
+
+{/*      <ul> {listItems} </ul> */}
+    <ul> {eventWidget(checked)} </ul>
+
+{/*       <div>
         <EventsList data={events} />
-      </div>
+      </div> */}
 
     </div>
   );
